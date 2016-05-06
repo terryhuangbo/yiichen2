4334041514977985f60a80d6a699be7e4579e15a
terry <terry1987101@163.com>
2016-05-07 03:07:52 +0800

    去掉一个h5属性，解决首页格式混乱的问题

--- wp-content/themes/ycafan/home.php
+++ wp-content/themes/ycafan/home.php
@@ -295,7 +295,7 @@ $_special_posts = $_special_posts ? $_special_posts
 </div>
 
 <div class="sbl row">
-    <div data-cmpt-autofixed data-autofixed-follow-to=".js-index-part-one">
+    <div >
         <div class="ifanr-sudu-border">
         </div>
         <div id="ifanr_side_jiong_widget-81" class="widget-container widget_ifanr_jiong_new clearfix">
