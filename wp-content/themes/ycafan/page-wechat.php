03192d41c2db53969640264337e30dc66eda9903
terry <terry1987101@163.com>
2016-05-07 17:39:05 +0800

    增加破土等的微信

--- wp-content/themes/ycafan/page-wechat.php
+++ wp-content/themes/ycafan/page-wechat.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: 微信
+ * Template Name: 微信页
  **/
 
 get_header();
@@ -9,27 +9,33 @@ get_header();
 <div class="weixin-caontainer-bg">
     <ol class="weixin-container">
         <li class="weixin-item">
-            <img class="app-logo"
-                 src="<?php bloginfo('template_url'); ?>/images/wechat-logo.jpg">
-
+            <img class="app-logo" src="<?php bloginfo('template_url'); ?>/images/wechat-logo.jpg">
             <div class="app-name">少年中国评论</div>
             <div class="app-weixin-code">公众号：少年中国评论</div>
-            <img class="app-qrcode-image"
-                 src="<?php bloginfo('template_url'); ?>/images/wechat.jpg">
-
+            <img class="app-qrcode-image" src="<?php bloginfo('template_url'); ?>/images/wechat.jpg">
             <div class="app-slogan">面对现实，忠于理想</div>
         </li>
-<!--        <li class="weixin-item">-->
-<!--            <img class="app-logo"-->
-<!--                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/appso.jpg">-->
-<!---->
-<!--            <div class="app-name">AppSolution</div>-->
-<!--            <div class="app-weixin-code">公众号：appsolution</div>-->
-<!--            <img class="app-qrcode-image"-->
-<!--                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/appsoQR.jpg">-->
-<!---->
-<!--            <div class="app-slogan">你唯一需要的数字生活解决方案</div>-->
-<!--        </li>-->
+        <li class="weixin-item">
+            <img class="app-logo" src="<?php bloginfo('template_url'); ?>/images/wechat-logo.jpg">
+            <div class="app-name">保马</div>
+            <div class="app-weixin-code">公众号：PourMarx</div>
+            <img class="app-qrcode-image" src="<?php bloginfo('template_url'); ?>/images/baoma-wechat.jpg">
+            <div class="app-slogan">保卫马克思</div>
+        </li>
+        <li class="weixin-item">
+            <img class="app-logo" src="<?php bloginfo('template_url'); ?>/images/wechat-logo.jpg">
+            <div class="app-name">破土</div>
+            <div class="app-weixin-code">公众号：破土</div>
+            <img class="app-qrcode-image" src="<?php bloginfo('template_url'); ?>/images/potu-wechat.png">
+            <div class="app-slogan">面对现实，忠于理想</div>
+        </li>
+        <li class="weixin-item">
+            <img class="app-logo" src="<?php bloginfo('template_url'); ?>/images/wechat-logo.jpg">
+            <div class="app-name">新汉之声</div>
+            <div class="app-weixin-code">公众号：liberty_wuhan</div>
+            <img class="app-qrcode-image" src="<?php bloginfo('template_url'); ?>/images/xinhan-wechat.jpg">
+            <div class="app-slogan">面对现实，忠于理想</div>
+        </li>
     </ol>
 </div>
 
