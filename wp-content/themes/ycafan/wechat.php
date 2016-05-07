ae0a04a461575591bdb5b6d8e3117c6ed9aa0684
terry <terry1987101@163.com>
2016-05-07 16:09:35 +0800

    添加二维码图片

---
+++ wp-content/themes/ycafan/wechat.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Template Name: 微信
+ **/
+
+get_header();
+?>
+<div class="weixin-caontainer-bg">
+    <ol class="weixin-container">
+        <li class="weixin-item">
+            <img class="app-logo"
+                 src="<?php bloginfo('template_url'); ?>/images/wechat-logo.jpg">
+
+            <div class="app-name">少年中国评论</div>
+            <div class="app-weixin-code">公众号：少年中国评论</div>
+            <img class="app-qrcode-image"
+                 src="<?php bloginfo('template_url'); ?>/images/wechat.jpg">
+
+            <div class="app-slogan">面对现实，忠于理想</div>
+        </li>
+        <li class="weixin-item">
+            <img class="app-logo"
+                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/appso.jpg">
+
+            <div class="app-name">AppSolution</div>
+            <div class="app-weixin-code">公众号：appsolution</div>
+            <img class="app-qrcode-image"
+                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/appsoQR.jpg">
+
+            <div class="app-slogan">你唯一需要的数字生活解决方案</div>
+        </li>
+<!--        <li class="weixin-item">-->
+<!--            <img class="app-logo"-->
+<!--                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/coolbuy.jpg">-->
+<!---->
+<!--            <div class="app-name">玩物志</div>-->
+<!--            <div class="app-weixin-code">公众号：coolbuy</div>-->
+<!--            <img class="app-qrcode-image"-->
+<!--                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/coolbuyQR.jpg">-->
+<!---->
+<!--            <div class="app-slogan">就是要你花钱</div>-->
+<!--        </li>-->
+<!--        <li class="weixin-item">-->
+<!--            <img class="app-logo"-->
+<!--                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/T2D.jpg">-->
+<!---->
+<!--            <div class="app-name">新时驱</div>-->
+<!--            <div class="app-weixin-code">公众号：time2drive</div>-->
+<!--            <img class="app-qrcode-image"-->
+<!--                 src="http://ifanr-cdn.b0.upaiyun.com/wp-content/themes/doge/static/images/app-qrcodes/T2DQR.jpg">-->
+<!---->
+<!--            <div class="app-slogan">随时了解汽车科技新动态</div>-->
+<!--        </li>-->
+    </ol>
+</div>
+
+<div class="u-clearfix"></div>
+<!-- 底部 -->
+<?php get_footer(); ?>
