43c86abb8e2ceb33419b0f0f45860779177728f0
terry <terry1987101@163.com>
2016-05-14 20:12:16 +0800

    删掉评论多余的代码

--- wp-content/themes/ycafan/comments.php
+++ wp-content/themes/ycafan/comments.php
@@ -17,7 +17,6 @@ if ( post_password_required() ) {
     return;
 }
 $commenter = wp_get_current_commenter();
-
 ?>
 
 <div class="o-single-content__body__content">
@@ -63,95 +62,6 @@ $commenter = wp_get_current_commenter();
             </nav>
 
             <ul class="c-article-comments__list js-comments-list">
-                <?php foreach ([1] as $key => $val): ?>
-                    <li id="comment-1014329" class="c-article-comments-item">
-                        <img class="c-article-comments-item__avatar" src="http://gravatar.ifanrx.com/avatar/41a0b1eac4c771512e23df01308d6925?s=80&amp;d=%2F%2Fifanr.b0.upaiyun.com%2Fsite-static%2Fifanr-2.0%2Fdist%2Fimages%2Fcommon%2Fgravatar.jpg" alt="luow">
-                        <div id="comment-cont-1014329" class="c-article-comments-item__info  js-single-comment" data-comment-username="luow" data-comment-id="1014329">
-                            <span class="c-article-comments-item__name">luow</span>
-                            <span class="c-article-comments-item__meta">2016-05-10 13:37:25</span>
-                            <span class="c-article-comments-item__hint js-show-comment">*评分过低，点击显示隐藏评论*</span>
-                            <div class="c-article-comments-item__content"><p>没有杀手级应用，就说明还是个玩具。</p></div>
-                            <button class="c-article-comments-item__reply js-reply-comment" data-post_id="<?php echo $post->ID ?>"
-                                    data-author="huangbo" data-email="1234@qq.com" data-comment_id="42">回复</button>
-                            <button class="c-article-comments-item-voting c-article-comments-item-voting--down js-vote-down ">2</button>
-                            <button class="c-article-comments-item-voting c-article-comments-item-voting--up js-vote-up ">0 </button>
-
-                        </div>
-                        <ul id="reply-content-1014329" class="c-article-comments-replies c-article-comments-item__info js-child-comments">
-                            <li id="comment-1014334" class="c-article-comments-replies__item  js-single-comment" data-comment-username="王 飞" data-comment-id="1014334">
-                                <div id="comment-cont-1014334" class="c-article-comments-replies__item__wrapper reply_item_li">
-                                    <span class="c-article-comments-item__name">
-                                        王 飞: <a class="comment-at" href="#comment-1014329" "="">@luow</a></span>
-                                    <span class="c-article-comments-item__meta">2016-05-10 13:47:15</span>
-                                    <div class="c-article-comments-item__hint c-article-comments-item__hint--secondary js-show-comment">
-                                        <span>评分过低，点击显示隐藏评论</span>
-                                    </div>
-                                    <div class="c-article-comments-item__content">
-                                        <p>其实对于 PS VR 来说，它始终就是个玩具，毕竟游戏方面占大头。</p>
-                                    </div>
-                                    <button class="c-article-comments-item__reply c-article-comments-replies__reply js-reply-comment"
-                                            data-post_id="<?php echo $post->ID ?>" author="zhengwenfagn" email="7890@qq.com" comment_id="44">回复</button>
-                                    <button class="c-article-comments-item-voting c-article-comments-item-voting--down js-vote-down ">0</button>
-                                    <button class="c-article-comments-item-voting c-article-comments-item-voting--up js-vote-up ">0</button>
-                                </div>
-                            </li>
-
-                            <li id="comment-130" class="c-article-comments-replies__item  js-single-comment"
-                                data-comment-username="ss" data-comment-id="130">
-                                <div class="c-article-comments-replies__item__wrapper reply_item_li"><span
-                                        class="c-article-comments-item__name">           ss: <a class="comment-at"
-                                                                                                href="#comment-130" "="">@ddd</a></span>
-                                    <span class="c-article-comments-item__meta">2016-05-13 18:38:14</span>
-
-                                    <div
-                                        class="c-article-comments-item__hint c-article-comments-item__hint--secondary js-show-comment">
-                                        <span>评分过低，点击显示隐藏评论</span></div>
-                                    <div class="c-article-comments-item__content"><p></p></div>
-                                    <button
-                                        class="c-article-comments-item__reply c-article-comments-replies__reply js-reply-comment">
-                                        回复
-                                    </button>
-                                    <button
-                                        class="c-article-comments-item-voting c-article-comments-item-voting--down js-vote-down ">
-                                        0
-                                    </button>
-                                    <button
-                                        class="c-article-comments-item-voting c-article-comments-item-voting--up js-vote-up ">
-                                        0
-                                    </button>
-                                </div>
-                            </li>
-
-
-                            <li id="comment-1014415" class="c-article-comments-replies__item  js-single-comment"
-                                data-comment-username="Jack" data-comment-id="1014415">
-                                <div class="c-article-comments-replies__item__wrapper"><span
-                                        class="c-article-comments-item__name">Jack: <a class="comment-at"
-                                                                                       href="#comment-1014329" "="">@luow</a></span>
-                                    <span class="c-article-comments-item__meta">2016-05-10 18:23:55</span>
-
-                                    <div
-                                        class="c-article-comments-item__hint c-article-comments-item__hint--secondary js-show-comment">
-                                        <span>评分过低，点击显示隐藏评论</span></div>
-                                    <div class="c-article-comments-item__content"><p>你希望一个游戏机的外设不是玩具是什么……</p>
-                                    </div>
-                                    <button
-                                        class="c-article-comments-item__reply c-article-comments-replies__reply js-reply-comment">
-                                        回复
-                                    </button>
-                                    <button
-                                        class="c-article-comments-item-voting c-article-comments-item-voting--down js-vote-down ">
-                                        0
-                                    </button>
-                                    <button
-                                        class="c-article-comments-item-voting c-article-comments-item-voting--up js-vote-up ">
-                                        0
-                                    </button>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                <?php endforeach ?>
 
             </ul>
 
