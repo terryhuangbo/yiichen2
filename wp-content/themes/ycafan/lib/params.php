8cb03e857a4fb3ceb738f78286a363a1128c1d5f
terry <terry1987101@163.com>
2016-05-28 18:35:29 +0800

    修改默认文章图片参数

--- wp-content/themes/ycafan/lib/params.php
+++ wp-content/themes/ycafan/lib/params.php
@@ -1,8 +1,11 @@
 <?php
 return [
-    '_menu_defautl_url' => get_bloginfo('template_url') . '/images/default-page-pic.png',
-    'memcache_host' => '127.0.0.1',
+	//memcached
+	'memcache_host' => '127.0.0.1',
     'memcache_port' => 11211,
+
+	//文章默认图片
+    'defautl_post_img' => get_bloginfo('template_url') . '/images/default-page-pic.png',    
     //关于我们
     'about_url' => 'http://www.yca.com/?page_id=2',
     //微信页面
