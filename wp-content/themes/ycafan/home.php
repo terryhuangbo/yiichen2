641cf11af90dd468da6ff4bbd44865c17beee376
bhuang <3121045133@qq.com>
2016-09-18 21:27:44 +0800

    【dev】暂时屏蔽首页评论列表

--- wp-content/themes/ycafan/home.php
+++ wp-content/themes/ycafan/home.php
@@ -343,7 +343,7 @@ $_special_posts = _get_index_specials($_special_slug, $_special_num);
             <?php endforeach ?>
         </div>
 
-        <?php if(intval(get_option('comment-open')) == 1):?>
+        <?php if(0 && intval(get_option('comment-open')) == 1):?>
             <div id="iycar_widget_buzz-3" class="widget-container widget_iycar_widget_buzz clearfix" style="display: block">
                 <div class="widget-buzz-container">
                     <div class="o-widget__header c-hot-comment-widget-header">
